7d9622e2f8f32b35d5e8b09df5fcd49e55a222e5
SamuelMM <samuelmangia@gmail.com>
2022-01-17 11:18:31 -0300

    Final project

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,22 @@
 import {Request, Response} from 'express';
+import {Pet} from '../models/pet';
+import {createMenuObject} from '../helpers/createMenuObjects';
+
 
 export const search = (req: Request, res: Response) =>{
+    let query: string = req.query.data as string;
     
+    if(!query){
+        res.redirect('/');
+        return;
+    };
+
+    let list = Pet.getFromname(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
+
 }
